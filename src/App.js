61505017e6987b2fceda7b9ef16e59e11013b32c
Liviu Preda <liviupreda@gmail.com>
2020-04-03 10:05:54 +0300

    add PageContent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "./Navbar";
 import Form from "./Form";
+import PageContent from "./PageContent";
 
 class App extends Component {
   render() {
