fcc57844c0dc8bca3817b4385dd2e31c867720eb
Liviu Preda <liviupreda@gmail.com>
2020-04-06 09:31:19 +0300

    replace fragment with PageContent

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import PageContent from "./PageContent";
 class App extends Component {
   render() {
     return (
-      <>
+      <PageContent>
         <Navbar />
         <Form />
-      </>
+      </PageContent>
     );
   }
 }
